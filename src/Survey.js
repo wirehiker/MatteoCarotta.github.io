b1f7f2b7a92407d51343102c444adb0d1053b8e3
Gott50 <xxGott50xx@gmail.com>
2019-01-28 19:36:18 +0100

    add Link

--- src/Survey.js
+++ src/Survey.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import survey from './img/Dashboard4.1--Survey.png';
+import {Link} from "react-router-dom";
 
 export default class Survey extends Component {
   render() {
     return (
-      <div>
+      <Link to="/properties">
           <img src={survey} style={{width: "100%"}}/>
-      </div>
+      </Link>
     );
   }
 }
