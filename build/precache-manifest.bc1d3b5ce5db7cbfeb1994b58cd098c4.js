6eea79187a98c2a50ae3ec411387e8c32d7bc9ee
MatteoCarotta <matteo.carotta@gmail.com>
2019-02-06 05:33:52 +0100

    Dashboard
    Dashboard implementation has begun!

---
+++ build/precache-manifest.bc1d3b5ce5db7cbfeb1994b58cd098c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "68eb81db07eaf2c42f11cf3f00f05654",
+    "url": "/static/media/Dashboard4.1--Survey.68eb81db.png"
+  },
+  {
+    "revision": "f5bc61d7549ac769284e55d2b45a0aa6",
+    "url": "/static/media/Dashboard4.1--Properties.f5bc61d7.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "91c912531338a81a76b8",
+    "url": "/static/js/main.91c91253.chunk.js"
+  },
+  {
+    "revision": "7677c6cd6d7b5e99e6e3",
+    "url": "/static/js/1.7677c6cd.chunk.js"
+  },
+  {
+    "revision": "91c912531338a81a76b8",
+    "url": "/static/css/main.16a4c27e.chunk.css"
+  },
+  {
+    "revision": "8cd873bbc76e3657f175669c480d3ab4",
+    "url": "/index.html"
+  }
+];
